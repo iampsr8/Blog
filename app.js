86e3156269c639fd7c2d938ca89ca3532e00cf0e
Prateek Rath <prateekrath28@gmail.com>
2022-07-08 01:02:03 +0530

    Deploy to mongoose

--- app.js
+++ app.js
@@ -9,7 +9,8 @@ const __dirname = url.fileURLToPath(new URL(".", import.meta.url));
 import express from "express";
 import bodyParser from "body-parser";
 import ejs from "ejs";
-import _ from 'lodash'
+import _ from "lodash";
+import mongoose from "mongoose";
 
 const homeStartingContent =
   "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -18,9 +19,18 @@ const aboutContent =
 const contactContent =
   "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
-const posts = [];
+mongoose.connect("mongodb://localhost:27017/blogDB");
+const postSchema = new mongoose.Schema({
+  title: String,
+  content: String,
+});
+
+const Post = mongoose.model("Post", postSchema);
+
+// const posts = [];
 
 const app = express();
+app.locals._ = _;
 
 app.set("view engine", "ejs");
 
@@ -28,17 +38,19 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  res.render("home", { startingContent: homeStartingContent, posts: posts });
+  Post.find({}, (err, posts) => {
+    res.render("home", { startingContent: homeStartingContent, posts: posts });
+  });
 });
 
-app.get('/posts/:postName', (req, res) => {
-  const title = _.lowerCase(req.params.postName);
-  posts.find(post => {
-    if (_.lowerCase(post.title) === title) {
-      res.render('post',{postTitle:post.title, postContent:post.content})
-    } 
-  })
-})
+app.get("/posts/:postId", (req, res) => {
+  const id = req.params.postId;
+  Post.findOne({ _id: id }, (err, post) => {
+    if (!err) {
+      res.render("post", { postTitle: _.capitalize(post.title), postContent: post.content });
+    }
+  });
+});
 
 app.get("/about", (req, res) => {
   res.render("about", { aboutContent: aboutContent });
@@ -53,12 +65,15 @@ app.get("/compose", (req, res) => {
 });
 
 app.post("/compose", (req, res) => {
-  const post = {
+  const post = new Post({
     title: req.body.postTitle,
     content: req.body.postBody,
-  };
-  posts.push(post);
-  res.redirect("/");
+  });
+  post.save((err) => {
+    if (!err) {
+      res.redirect("/");
+    }
+  });
 });
 
 app.listen(3000, function () {
